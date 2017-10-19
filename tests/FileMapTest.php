0357350ae1636a5af5f9a2035d8e3797c4a671ef
Andrii Afanasiev <andrii@crytek.de>
2017-10-19 09:35:37 +0200

    remove commented code

--- tests/FileMapTest.php
+++ tests/FileMapTest.php
@@ -6,25 +6,9 @@ class FileMapTest
 {
     public function testTheme()
     {
-//        $list = \oxcom\phpbbch\core\settings::getThemes();
-//
-//        foreach (array_keys($list) as $file) {
-//            $path = __DIR__ . '/../styles/all/theme/higlightjs/' . $file . '.min.css';
-//            if (!file_exists($path)) {
-//                var_dump('Not found: ' . $file . ' by path: ' . $path);
-//            }
-//        }
     }
 
     public function testLanguage()
     {
-//        $list = \oxcom\phpbbch\core\settings::getLanguages();
-//
-//        foreach (array_keys($list) as $file) {
-//            $path = __DIR__ . '/../styles/all/template/js/higlightjs/languages/' . $file . '.min.js';
-//            if (!file_exists($path)) {
-//                var_dump('Not found: ' . $file . ' by path: ' . $path);
-//            }
-//        }
     }
 }
